d5ab446031d3d2e69e92f80e759563bba525aa35
saurabhfegade <saurabh.fegade1998@gmail.com>
2022-04-15 15:31:48 +0530

    deployed to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,36 @@
-require('babel-register');
-require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require("babel-register");
+require("babel-polyfill");
+
+const MNEMONIC = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://rinkeby.infura.io/v3/992f0efc79f64bee82609722d3c68057"
+        );
+      },
+      network_id: 4,
+      gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
     },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 200,
       },
-      evmVersion: "petersburg"
-    }
-  }
-}
+      evmVersion: "petersburg",
+    },
+  },
+};
